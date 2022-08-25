25e2859a12c00902110e64c7d8ffa6c60b759198
Lucas-Melo0 <lucassss8v@gmail.com>
2022-08-25 15:57:27 -0300

    [feat]: Add get request to tweets endpoint

--- index.js
+++ index.js
@@ -16,7 +16,22 @@ server.post("/sign-up", (req, res) => {
 server.post("/tweets", (req, res) => {
   const tweet = req.body;
   tweets.push(tweet);
+  console.log(tweets);
   res.send("OK");
 });
-
+server.get("/tweets", (req, res) => {
+  let lastTenTweets = [];
+  for (let i = tweets.length - 1; i > tweets.length - 11; i--) {
+    lastTenTweets.push(tweets[i]);
+    console.log(lastTenTweets);
+  }
+  const latestTweets = lastTenTweets.map((value) => {
+    const avatar = users.find((user) => {
+      return user.username === value.username;
+    })?.avatar;
+    return { ...value, avatar: avatar };
+  });
+  res.send(latestTweets);
+});
+console.log(tweets);
 server.listen(5000);
