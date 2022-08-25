46bd0da17d537fe4e28a59e6aa800c1efbd5cfe3
Lucas-Melo0 <lucassss8v@gmail.com>
2022-08-25 08:44:54 -0300

    [feat]: Add post request to sign-up endpoint

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 import express from "express";
 
 const server = express();
+
+server.use(express.urlencoded({ extended: false }));
+server.use(express.json());
+const users = [];
+
+server.post("/sign-up", (req, res) => {
+  const user = req.body;
+  users.push(user);
+  res.send("OK");
+});
+
+server.listen(5000);
