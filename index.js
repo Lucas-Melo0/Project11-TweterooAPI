5e7dcc157620d1aa6e67a086c64c834ed14c3ee0
Lucas-Melo0 <lucassss8v@gmail.com>
2022-08-25 08:50:54 -0300

    [feat]: Add post request to tweets endpoint

--- index.js
+++ index.js
@@ -5,10 +5,17 @@ const server = express();
 server.use(express.urlencoded({ extended: false }));
 server.use(express.json());
 const users = [];
+const tweets = [];
 
 server.post("/sign-up", (req, res) => {
   const user = req.body;
   users.push(user);
+  res.send("OK");
+});
+
+server.post("/tweets", (req, res) => {
+  const tweet = req.body;
+  tweets.push(tweet);
   res.send("OK");
 });
 
